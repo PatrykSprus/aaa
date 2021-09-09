78482f106fce91a9b711eb8a0e962a95c9e2edfd
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2021-09-09 22:19:43 +0200

    .
    .

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mysql-patryksprus.alwaysdata.net";
+$username = "217136";
+$password = "@H@R8He5TsaaXJR";
+$dbname = "patryksprus_baza";
+$conn = new mysqli($servername, $username, $dbname);
+?>
+
+
+
+
+
+
+
